4acfa4b175b33195ee134115cfc409698635b857
maomao <2020377497@qq.com>
2021-11-27 18:07:12 +0800

    Site updated: 2021-11-27 18:07:11

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// menu
+const navigator = document.querySelector(".navigator")
+const menu = document.querySelector(".menu")
+const main = document.querySelector(".main")
+
+if (menu !== null) {
+  menu.addEventListener("click", () => {
+    navigator.classList.add("fixed")
+    main.classList.add("fixed")
+  }, true)
+  main.addEventListener("click", () => {
+    navigator.classList.remove("fixed")
+    main.classList.remove("fixed")
+  }, true)
+}
+// mediumZoom(".markdown-body img")
+mediumZoom(".article.post .markdown-body img,.post .describe>.img img")
+
+// hljs.initHighlightingOnLoad()
+hljs.initHighlightingOnLoad()
+
+
+const postImgs = document.querySelectorAll(".article.post .markdown-body img")
+postImgs.forEach((item) => {
+  item.setAttribute("data-src", item.src)
+  item.removeAttribute("src")
+  item.classList.add("lazyload", "blur-up")
+})
